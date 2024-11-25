bc3c5c469228e28f2d0d06c5903676107a662fb3
ole-jonas <olejonaskoder@gmail.com>
2024-11-25 14:43:59 +0100

    Button

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello!');
+const konverteringButton = document.querySelector('#konverter-button')
+
+const numberInput = document.querySelector("#number")
+
+function konvertering (event) {
+    event.preventDefault() 
+  
+    const number = numberInput.value
+  
+    const feet = number * 3.2
+  
+    konverteringButton.textContent = number + " meter = " + feet + " feet"
+  
+  }
+  
+  konverteringButton.addEventListener("click", konvertering)
+  submitBtn.addEventListener('click', submit);
