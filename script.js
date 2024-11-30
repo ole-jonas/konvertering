ceb1bceefcfcdf4979bf2492c572a59ecfc8ca86
ole-jonas <olejonaskoder@gmail.com>
2024-11-30 16:46:47 +0100

    Div verdier

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ document.getElementById('konverter-button').addEventListener('click', function()
     // Convert inches to centimeters (1 inch = 2.54 cm)
     const centimeters = (inches * 2.54).toFixed(2);
     
+     // Convert inches to meters (1 inch = 0.0254 cm)
+     const meters = (inches * 0.0254).toFixed(2);
     // Display the result
-    document.getElementById('result').textContent = `${inches} tommer er lik ${centimeters} cm.`;
+    document.getElementById('result').textContent = `${inches} tommer er lik ${centimeters} cm og ${meters} m.`;
     document.getElementById('result-container').style.display = 'block';
   } else {
     alert('Vennligst skriv inn et gyldig nummer.');
